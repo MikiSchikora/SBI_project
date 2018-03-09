d3f677611c62b145b526c64a694bd8f0c4eca898
MikiSchikora <miki.s.t@hotmail.com>
2018-03-09 15:34:45 +0100

    New PDBs

--- Generate_pairwise_interactions.py
+++ Generate_pairwise_interactions.py
@@ -7,6 +7,7 @@ from Bio.PDB import Structure as pdb_struct
 from Bio.PDB import Model as pdb_model
 import numpy as np
 import os
+import copy
 
 parser = pdb.PDBParser(PERMISSIVE=1)
 
@@ -17,13 +18,23 @@ model = structure[0]
 # free the ./TEMPLATES_DIR/
 os.system('rm -rf ./TEMPLATES/*')
 
+# initialize the saved pairs
+saved_pairs = set()
+
 for chain1 in model.get_chains():
 
    #new_structure = chain1
 
    for chain2 in model.get_chains():
 
+      #comb = chain1.id+'.'+chain2.id
+      #comb_rev = chain2.id+'.'+chain1.id
+
       if chain1 is not chain2:
+
+         # save the combination
+         #saved_pairs.add(comb)
+         #saved_pairs.add(comb_rev)
 
          # ask if any of the residues is interacting, if so save the PDB
 
@@ -40,7 +51,7 @@ for chain1 in model.get_chains():
                   except KeyError:
                      ## no CA atom, e.g. for H_NAG
                      continue
-                  if distance < 6:
+                  if distance < 15:
                      chains_interacting = 1
 
          if chains_interacting==1:
@@ -48,9 +59,10 @@ for chain1 in model.get_chains():
             # create a structure object
             ID = chain1.id+'_and_'+chain2.id
             new_structure = pdb_struct.Structure(ID)
+
             new_model = pdb_model.Model(0)
-            new_model.add(chain1)
-            new_model.add(chain2)
+            new_model.add(copy.deepcopy(chain1))
+            new_model.add(copy.deepcopy(chain2))
             new_structure.add(new_model)
 
             # move the coordinates of the structure to simulate what would happen if they were coming from different files
@@ -59,7 +71,10 @@ for chain1 in model.get_chains():
             translation = np.array((0, 0, 1), 'f')
 
             for atom in new_structure.get_atoms():
-               atom.transform(rotation, translation)
+               #
+               #
+               #atom.transform(rotation, translation)
+               pass
 
             # write to new pdb:
 
