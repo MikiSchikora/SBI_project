9aa44b8d8dec5294c3249f240d5b6fc89ba9c4d8
marinallealcustey <marina.lleal01@estudiant.upf.edu>
2018-04-03 19:19:25 +0200

    add setup

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
-__all__ = ['Functions']
+from distutils.core import setup
+
+setup(name='BYCProject', version='1.0', description='Build a biological macrocomplex from pairwise interactions',
+	author='Miquel Angel Schikora Tamarit and Marina Lleal Custey',
+	author_email='miquelangel.schikora01@estudiant.upf.edu, marina.lleal01@estudiant.upf.edu',
+	url='https://github.com/MikiSchikora/SBI_project',
+	packages=['byc'])
